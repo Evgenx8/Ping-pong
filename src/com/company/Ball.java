f5b4d6fddb3aa1c9bc3e7d552b5fb7b2466a57b1
evgeniy <esafronov@megalabs.ru>
2023-11-29 11:47:42 +0300

    12345

--- src/com/company/Ball.java
+++ src/com/company/Ball.java
@@ -9,7 +9,7 @@ public class Ball {
 
     private int x, y; // положение
     private int xVel, yVel; // 1 или -1
-    private int speed = 8; // скорость мяча
+    private int speed = 8; // скорость мяча 12312423234123
 
     //конструктор
     public Ball() {
